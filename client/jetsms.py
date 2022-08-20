43c614100cf83bd73722e9ebd0927b5ae502b9d8
Uğur Okumuş <ugur.okumus@biotekno.com.tr>
2022-08-21 01:47:46 +0300

    readme

--- client/jetsms.py
+++ client/jetsms.py
@@ -13,8 +13,8 @@ class JetSmsSingle:
 		self.text = text
 
 	def send(self,title,url="https://service.jetsms.com.tr/SMS-Web8/xmlsms",startdate="",expiredate=""):
-		data = '<?xml version="1.0" encoding="utf-8" ?><message-context type="smmgsd" ><username>'+self.username+'</username><password>'+self.password+'</password><outbox-name>'+title+'</outbox-name><reference></reference><start-date>'+startdate+'</start-date><expire-date>'+expiredate+'</expire-date><message-header></message-header><text>'+self.text+'</text><gsmnos>'+self.gsm+'</gsmnos></message-context>'
-		request = urllib2.Request(url, data)
+		self.data = '<?xml version="1.0" encoding="utf-8" ?><message-context type="smmgsd" ><username>'+self.username+'</username><password>'+self.password+'</password><outbox-name>'+title+'</outbox-name><reference></reference><start-date>'+startdate+'</start-date><expire-date>'+expiredate+'</expire-date><message-header></message-header><text>'+self.text+'</text><gsmnos>'+self.gsm+'</gsmnos></message-context>'
+		request = urllib2.Request(url, self.data)
 		request.add_header('Content-Type', 'text/xml')
 		response = urllib2.urlopen(request)
 		responseText = response.read()
